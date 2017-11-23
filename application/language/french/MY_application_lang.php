bc9cffb5b3ecbc71b1dc096ba446ddf1d812db3f
Didier Viret <didier.viret@orif.ch>
2017-11-23 21:36:59 +0100

    Base packages

---
+++ application/language/french/MY_application_lang.php
@@ -0,0 +1,36 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Application's french text
+ *
+ * @author      Didier Viret
+ * @link        https://github.com/DidierViret
+ * @copyright   Copyright (c) 2017, Didier Viret
+ */
+
+// Application name
+$lang['app_title']                      = 'CI Test';
+
+// Page titles
+$lang['page_prefix']                    = 'CI Test';
+
+// Date and time formats
+$lang['date_format_short']              = 'd.m.Y';
+$lang['datetime_format_short']          = 'd.m.Y H:i';
+
+// Fields labels
+$lang['field_username']                 = 'Identifiant';
+
+// List headers
+$lang['header_picture']                 = 'Photo';
+
+// Buttons
+$lang['btn_admin']                      = 'Administration';
+
+// Messages
+$lang['msg_no_item']                    = 'Aucun objet à afficher';
+
+// Bootstrap labels
+$lang['lbl_loan_status_loaned']         = 'En prêt';
+
+// Other texts
+$lang['text_login']                     = 'Connexion';
---
+++ application/views/common/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Copied from Bootstrap model (http://getbootstrap.com/getting-started/) -->
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title><?php
+        if (is_null($title) || $title == '') {
+            echo $this->lang->line('page_prefix');
+        } else {
+            echo $this->lang->line('page_prefix').' - '.$title;
+        }
+    ?></title>
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="<?php echo base_url("assets/images/favicon.ico"); ?>" type="image/x-icon" />
+
+    <!-- Bootstrap styles -->
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
