c95dd4e926698e142155ddbaf9d860c6dbaee193
Didier Viret <didier.viret@orif.ch>
2017-11-25 17:23:57 +0100

    Added database script

---
+++ database/ci_base.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  sam. 25 nov. 2017 à 17:23
+-- Version du serveur :  10.1.28-MariaDB
+-- Version de PHP :  7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `ci_base`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_user`
+--
+
+CREATE TABLE `t_user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `fk_user_group` int(10) UNSIGNED NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `date_creation` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `t_user`
+--
+
+INSERT INTO `t_user` (`id`, `fk_user_group`, `username`, `password`, `first_name`, `last_name`, `email`, `date_creation`) VALUES
+(1, 1, 'admin', NULL, NULL, 'Administrateur', NULL, '2017-11-25'),
+(2, 2, 'user', NULL, NULL, 'Utilisateur', NULL, '2017-11-25');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_user_group`
+--
+
+CREATE TABLE `t_user_group` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `level` int(10) UNSIGNED NOT NULL,
+  `date_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `t_user_group`
+--
+
+INSERT INTO `t_user_group` (`id`, `name`, `level`, `date_creation`) VALUES
+(1, 'Administrateur', 10, '2017-11-25'),
+(2, 'Utilisateur', 1, '2017-11-25');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_group` (`fk_user_group`);
+
+--
+-- Index pour la table `t_user_group`
+--
+ALTER TABLE `t_user_group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `t_user`
+--
+ALTER TABLE `t_user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `t_user_group`
+--
+ALTER TABLE `t_user_group`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD CONSTRAINT `fk_user_group` FOREIGN KEY (`fk_user_group`) REFERENCES `t_user_group` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
